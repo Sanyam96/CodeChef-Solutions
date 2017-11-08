8084e77ff3bc968fb9ed8a5a46b47ca66a7d9d58
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-11-08 23:18:36 +0530

    Find Remainder - FLOW002

---
+++ Practice/Beginner/FLOW002.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(int argc, char const *argv[]){
+ 
+    int tc;
+    cin >> tc;
+    
+    while(tc--) {
+    	int a, b;
+    	cin >> a >> b;
+    	
+    	cout << a % b << endl;
+    }
+ 
+    return 0;
+}
