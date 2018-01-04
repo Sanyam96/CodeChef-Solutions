0d244aef13dc4f0486a6fd138b7bcd73426a9685
Sanyam96 <sanyam.bvcoe96@gmail.com>
2018-01-04 23:44:46 +0530

    MUFFINS3

---
+++ Practice/Beginner/MUFFINS3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(int argc, char const *argv[]){
+ 
+    int tc;
+    cin >> tc;
+    while(tc--) {
+    	int n;
+    	cin >> n;
+    	cout << n/2+1 << endl;
+    }
+    return 0;
+}
+     
