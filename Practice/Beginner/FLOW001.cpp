059c18a169db634881b05bb44935570c95572528
Sanyam96 <sanyam.bvcoe96@gmail.com>
2018-01-05 23:38:10 +0530

    FLOW001

---
+++ Practice/Beginner/FLOW001.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ 
+int main() {
+ 
+    int tc;
+    cin >> tc;
+    while( tc-- ) {
+    	int a;
+    	int b;
+    	cin >> a >> b;
+    	cout << a+b << "\n";
+    }
+    return 0;
+}
